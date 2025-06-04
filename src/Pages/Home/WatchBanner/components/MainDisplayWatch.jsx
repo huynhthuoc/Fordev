7cabedd0a29dd24ba43acebadcaf77fe55d061f0
huynhthuoc <huynhvanthuoc.e@gmail.com>
2025-06-04 22:36:03 +0700

    update file MainDisplayWatch

--- src/Pages/Home/WatchBanner/components/MainDisplayWatch.jsx
+++ src/Pages/Home/WatchBanner/components/MainDisplayWatch.jsx
@@ -4,21 +4,21 @@ import '../styles/MainDisplayWatch.scss';
 
 //opstion  date
 const DATE_FORMAT_OPTIONS = {
-  month: 'short',
   day: '2-digit',
+  month: 'short',
   weekday: 'short',
 };
 const TIME_FORMAT_OPTIONS = { hour: '2-digit', minute: '2-digit' };
 const MainDisplayWatch = () => {
   const [dates, setDates] = useState({
-    date: getDate(DATE_FORMAT_OPTIONS).toUpperCase(),
+    date: getDate(DATE_FORMAT_OPTIONS).toUpperCase().split(','),
     time: getDate(TIME_FORMAT_OPTIONS),
   });
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setDates({
-        date: getDate(DATE_FORMAT_OPTIONS).toUpperCase(),
+        date: getDate(DATE_FORMAT_OPTIONS).toUpperCase().split(','),
         time: getDate(TIME_FORMAT_OPTIONS),
       });
     }, 1000);
@@ -29,11 +29,14 @@ const MainDisplayWatch = () => {
   }, []);
   return (
     <div className="health-mainDisplayWatch">
-      <div className="mainDisplayWatch-date">{dates.date}</div>
+      <div className="mainDisplayWatch-date">
+        <p>{dates.date[1]}</p>
+        <p>{dates.date[0]}</p>
+      </div>
       <div className="mainDisplayWatch-time">
         <h2>{dates.time}</h2>
       </div>
-      <div className="mainDisplayWatch-circle"></div>
+      <div className="mainDisplayWatch-circle">s</div>
     </div>
   );
 };
